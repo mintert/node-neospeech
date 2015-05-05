e2eddcaabd59ad8c2d6efe2600f4af281f741bde
Lorenz Mintert <lorenz@mintert.org>
2015-05-05 15:38:43 +0200

    Update neospeech VTAP API

--- binding/libttsapi/libttsapi.h
+++ binding/libttsapi/libttsapi.h
@@ -1,11 +1,12 @@
-#ifndef _LIBTTSAPI_H_
-#define _LIBTTSAPI_H_
+/*
+ * Copyright (c) 2000-2014 Voiceware Co., Ltd., All rights reserved.
+ * 
+ * VTAP API (VoiceText Access Protocol API)
+*/
 
 
-#define __VTAP_VER__	3.11.5.3
+#define __VTAP_VER__	3.11.6.3
 
-
-#define DllExport		extern
 
 /* return code */
 #define TTS_HOSTNAME_ERROR		-1
@@ -43,14 +44,19 @@
 #define TTS_HYUNA_DB			15
 #define TTS_JIMIN_DB			17
 #define TTS_JIHUN_DB			18
+#define	TTS_SENA_DB			19
+#define TTS_YURA_DB			20
 #define TTS_KATE_DB			100
 #define TTS_PAUL_DB			101
 #define TTS_JULIE_DB			103
 #define TTS_JAMES_DB			104
+#define TTS_ASHLEY_DB			105
 #define TTS_LILY_DB			200
 #define TTS_WANG_DB			201
 #define TTS_HUI_DB			202
 #define TTS_LIANG_DB			203
+#define TTS_HONG_DB			204
+#define TTS_QIANG_DB			205
 #define TTS_MIYU_DB			300
 #define TTS_SHOW_DB			301
 #define TTS_MISAKI_DB			302
@@ -58,8 +64,12 @@
 #define TTS_SAYAKA_DB			304
 #define TTS_RYO_DB			305
 #define TTS_HIKARI_DB			306
+#define TTS_TAKERU_DB			307
 #define TTS_VIOLETA_DB			400
+#define TTS_FRANCISCO_DB		401
+#define TTS_GLORIA_DB			402
 #define TTS_BRIDGET_DB			500
+#define TTS_HUGH_DB			501
 #define TTS_CHLOE_DB			600
 
 // Voice Format Info
@@ -109,40 +119,61 @@ typedef struct
 typedef char* TTSKEY;
 
 #if defined(__cplusplus)
-    extern "C"
-    {
+	extern "C" {
 #endif
+
+#if !defined(DllExport)
+	#if defined(WIN32) || defined(WIN64)
+		#define DllExport       __declspec( dllexport )		
+	#else
+		#define DllExport       extern
+		
+	#endif
+#endif
+
+#if defined(WIN32) || defined(WIN64)
+	#include <Winsock2.h>		
+	#include <windows.h>
+#else
+	#define SOCKET	int
+	#define	INVALID_SOCKET	-1
+#endif
+
 /************************************************************/
 /* TTS Plain text APIs					    */
 /************************************************************/
-DllExport int TTSRequestFile(char *szServer, int nPort, char *pText, int nTextLen, char *szSaveDir, char *szSaveFile, int nSpeakerID, int nVoiceFormat);
+DllExport int TTSRequestFile(char* szServer, int nPort, char* pText, int nTextLen, char* szSaveDir, char* szSaveFile, int nSpeakerID, int nVoiceFormat);
 DllExport int TTSRequestFileSSML(char *szServer, int nPort, char *pText, int nTextLen, char *szSaveDir, char *szSaveFile, int nSpeakerID, int nVoiceFormat, int *pMarkSize, TTSMARK** ppTTSMark, int *pVoiceLen);
 
-DllExport int TTSRequestFileEx(char *szServer, int nPort, char *pText, int nTextLen, char *szSaveDir, char *szSaveFile, int nSpeakerID, int nVoiceFormat, int nTextFormat, int nVolume, int nSpeed, int nPitch, int nDictIndex);
+DllExport char* _TTSRequestBuffer(SOCKET* sockfd, char* szServer, int nPort, char* pText, int nTextLen, int* nVoiceLen, int nSpeakerID, int nVoiceFormat, int bFirst, int bAll, int* nReturn, TTSKEY* szkey);
 
-DllExport char *_TTSRequestBuffer(int *sockfd, char *szServer, int nPort, char *pText, int nTextLen, int *nVoiceLen, int nSpeakerID, int nVoiceFormat, int bFirst, int bAll, int *nReturn, TTSKEY *szkey);
-DllExport char *_TTSRequestBufferEx(int *sockfd, char *szServer, int nPort, char *pText, int nTextLen, int *nVoiceLen, int nSpeakerID, int nVoiceFormat, int nTextFormat, int nVolume, int nSpeed, int nPitch, int nDictIndex, int bFirst, int bAll, int *nReturn, TTSKEY *szkey);
+// expanded format functions, for volume, speed, pitch, dictionary number
+DllExport int TTSRequestFileEx(char* szServer, int nPort, char* pText, int nTextLen, char* szSaveDir, char* szSaveFile, int nSpeakerID, int nVoiceFormat, int nTextFormat, int nVolume, int nSpeed, int nPitch, int nDictNum);
+
+DllExport char* _TTSRequestBufferEx(SOCKET* sockfd, char* szServer, int nPort, char* pText, int nTextLen, int* nVoiceLen, int nSpeakerID, int nVoiceFormat, int nTextFormat, int nVolume, int nSpeed, int nPitch, int nDictNum, int bFirst, int bAll, int* nReturn, TTSKEY* szkey);
 
 /************************************************************/
 /* TTS SSML text APIs					    */
 /************************************************************/
-DllExport char *_TTSRequestBufferSSMLEx(int *sockfd, char *szServer, int nPort, char *pText, int nTextLen, int *nVoiceLen, int nSpeakerID, int nVoiceFormat, int nVolume, int nSpeed, int nPitch, int nDictIndex, int* pMarkSize, TTSMARK** ppTTSMark, int bFirst, int *nReturn, TTSKEY *szkey);
+DllExport char* _TTSRequestBufferSSMLEx(SOCKET* sockfd, char* szServer, int nPort, char* pText, int nTextLen, int* nVoiceLen, int nSpeakerID, int nVoiceFormat, int nVolume, int nSpeed, int nPitch, int nDictNum, int* pMarkSize, TTSMARK** ppTTSMark, int bFirst, int* nReturn, TTSKEY* szkey);
+DllExport void TTSBufferFree(char* pBuffer);
 
 /************************************************************/
 /* Admin Status TTS APIs				    */
 /************************************************************/
-DllExport int TTSRequestStatus(char *szServer, int nPort);
+DllExport int TTSRequestStatus(char* szServer, int nPort);
+DllExport void TTSSetTimeout(int nConnTime, int nReadWriteTime);
 
 /************************************************************/
 /* deprecated APIs					    */
 /************************************************************/
-DllExport char *TTSRequestBuffer(int *sockfd, char *szServer, int nPort, char *pText, int nTextLen, int *nVoiceLen, int nSpeakerID, int nVoiceFormat, int bFirst, int bAll, int *nReturn);
-DllExport char *TTSRequestBufferEx(int *sockfd, char *szServer, int nPort, char *pText, int nTextLen, int *nVoiceLen, int nSpeakerID, int nVoiceFormat, int nTextFormat, int nVolume, int nSpeed, int nPitch, int nDictIndex, int bFirst, int bAll, int *nReturn);
-DllExport char *TTSRequestBufferSSMLEx(int *sockfd, char *szServer, int nPort, char *pText, int nTextLen, int *nVoiceLen, int nSpeakerID, int nVoiceFormat, int nVolume, int nSpeed, int nPitch, int nDictIndex, int* pMarkSize, TTSMARK** ppTTSMark, int bFirst, int *nReturn);
+DllExport char* TTSRequestBuffer(SOCKET* sockfd, char* szServer, int nPort, char* pText, int nTextLen, int* nVoiceLen, int nSpeakerID, int nVoiceFormat, int bFirst, int bAll, int* nReturn);
+DllExport char* TTSRequestBufferEx(SOCKET* sockfd, char* szServer, int nPort, char* pText, int nTextLen, int* nVoiceLen, int nSpeakerID, int nVoiceFormat, int nTextFormat, int nVolume, int nSpeed, int nPitch, int nDictIndex, int bFirst, int bAll, int* nReturn);
+DllExport char* TTSRequestBufferSSMLEx(SOCKET* sockfd, char* szServer, int nPort, char* pText, int nTextLen, int* nVoiceLen, int nSpeakerID, int nVoiceFormat, int nVolume, int nSpeed, int nPitch, int nDictNum, int* pMarkSize, TTSMARK** ppTTSMark, int bFirst, int* nReturn);
+
+DllExport int InitSocket();
+DllExport int RemoveSocket();
 
 #if defined(__cplusplus)
     }
-#endif
-
-
 #endif /*_LIBTTSAPI_H_*/
